7cbbf1162844eca0798be13678bf7690690b1b63
Jesus Abarca <jesus.abarca@gompersstudent.org>
2019-03-11 14:25:37 -0700

    March 11

--- src/SnowFall.java
+++ src/SnowFall.java
@@ -1,3 +1,5 @@
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
 
 public class SnowFall {
@@ -7,21 +9,31 @@ public class SnowFall {
 	final int width= 500;
 	final int height= 800;
 	
+	GamePanel game;
+	
 	public static void main(String[] args) {
 	SnowFall snow = new SnowFall();
 	
 	snow.setup();
 	
+	}
+	
 	SnowFall(){
 	frame = new JFrame();
+	
+	game = new GamePanel();
+	
 	}
-
-	
-	
-}
 	
 	void setup(){
+		frame.add(game);
+		frame.addKeyListener(game);
+		frame.getContentPane().setPreferredSize(new Dimension(width, height));
+
+        frame.pack();
+        frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		game.startGame();
 	}
 	
 }
